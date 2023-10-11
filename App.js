dd67c204c18a092d2305f8875f1d97f93e517875
DioGuest <75037396+DioGuest@users.noreply.github.com>
2023-10-11 15:22:26 -0300

    Orgs-Cesta ( React native - Expo )

---
+++ App.js
@@ -0,0 +1,31 @@
+
+import { Montserrat_400Regular, Montserrat_700Bold, useFonts } from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+import mock from './src/mocks/cesta';
+import Cesta from './src/telas/Cesta';
+
+
+
+export default function App() {
+
+  const [fonteCarregada]= useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold" : Montserrat_700Bold,
+  });
+
+  if(!fonteCarregada){
+    return <AppLoading />
+  }
+
+  return (
+    <SafeAreaView style={{flex:1}}>
+      <StatusBar />
+      <Cesta {...mock} />
+      
+    </SafeAreaView>
+  );
+}
+
+
